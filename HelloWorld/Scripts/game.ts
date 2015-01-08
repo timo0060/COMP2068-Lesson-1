43fb37d64f74e9fcc9c379c627bf9d6b7df4475b
timo0060 <timo0060@gmail.com>
2015-01-08 13:34:12 -0500

    Added Basic structure

---
+++ HelloWorld/Scripts/game.ts
@@ -0,0 +1,17 @@
+﻿class Player {
+    strength: number;
+    constructor() {
+        this.strength = 10;
+    }
+
+    jump() {
+        console.debug("You've Jumped");
+    }
+} 
+
+function main() {
+
+    var tim = new Player();
+    tim.jump();
+
+}
